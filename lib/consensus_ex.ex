39b63dbabafdcb407461af0180435c1750fa973e
dgigafox <darren@mergecommit.com>
2019-03-01 18:23:18 +0800

    ✨ Add send and receive message

--- lib/consensus_ex.ex
+++ lib/consensus_ex.ex
@@ -7,32 +7,50 @@ defmodule ConsensusEx do
   if it comes from the database, an external API or others.
   """
 
-  use GenServer
+  # use GenServer
 
-  def start_link(default) when is_list(default) do
-    GenServer.start_link(__MODULE__, default)
+  # def start_link(default) when is_list(default) do
+  #   GenServer.start_link(__MODULE__, default)
+  # end
+
+  # def pop(pid) do
+  #   GenServer.call(pid, :pop)
+  # end
+
+  # # Server
+
+  # def init(initial_param) do
+  #   {:ok, initial_param}
+  # end
+
+  # def handle_call(:pop, from, [head | tail]) do
+  #   IO.inspect(from)
+  #   IO.inspect(head)
+  #   IO.inspect(tail)
+  #   {:reply, head, tail}
+  # end
+
+  # def handle_info(:work, state) do
+  #   IO.inspect(state, label: "STATE")
+  #   {:noreply, state}
+  # end
+
+  def receive_message(message) do
+    IO.puts(message)
   end
 
-  def pop(pid) do
-    GenServer.call(pid, :pop)
+  def send_message(recipient, message) do
+    spawn_task(__MODULE__, :receive_message, recipient, [message])
   end
 
-  # Server
-
-  def init(initial_param) do
-    send(:"foo@iMac-2", {:pop, [:hi]})
-    {:ok, initial_param}
+  def spawn_task(module, fun, recipient, args) do
+    recipient
+    |> remote_supervisor()
+    |> Task.Supervisor.async(module, fun, args)
+    |> Task.await()
   end
 
-  def handle_call(:pop, from, [head | tail]) do
-    IO.inspect(from)
-    IO.inspect(head)
-    IO.inspect(tail)
-    {:reply, head, tail}
-  end
-
-  def handle_info(message, state) do
-    IO.inspect(message)
-    {:noreply, state}
+  defp remote_supervisor(recipient) do
+    {ConsensusEx.TaskSupervisor, recipient}
   end
 end
